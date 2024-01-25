40dace916fceac3031113590c4e8f024b0aff92d
Grickham000 <guru.aalfa@gmail.com>
2024-01-25 15:44:33 -0600

    Adding services structure and initiating services for Project API

---
+++ scrum_builder_front/src/Services/ProjectsServices.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const getProject = async id =>{
+    const token = Cookies.get('jwt_token');
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const response = await axios.get(`'https://localhost:7041/api/Projects/${id}'`, config);
+    return response.data;
+};
+
+export default {
+    getProject
+}
